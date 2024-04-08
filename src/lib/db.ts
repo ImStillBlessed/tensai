479a1251822a5f65ce1fbaa6e8439479a6ab8075
Oigbochie Blessed <eprince1800@gmail.com>
2024-04-08 15:04:15 +0100

    fucking schema is not working ill ignore it and move on to the basic functionality of the app

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -14,23 +14,3 @@ if (process.env.NODE_ENV == "production") {
   }
   prisma = global.cachedPrisma;
 }
-
-// let prisma: PrismaClient;
-// declare global {
-//   namespace NodeJS {
-//     interface Global {
-//       prisma: PrismaClient;
-//     }
-//   }
-// }
-
-// if (process.env.NODE_ENV == "production") {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
-// export default prisma;
