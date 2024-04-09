895a07ecaac0e55dbdc026fd4085fbfd254fedb8
Oigbochie Blessed <eprince1800@gmail.com>
2024-04-09 16:44:13 +0100

    updated the schema for the game and questions setting up the gpt api route and making the game functionality problems: the schema is not working for the game model enum does not work for mongodbs the gpt api is not tested yet

---
+++ src/app/api/game/route.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const session = await getAuthSession();
+    // if (!session) {
+    //   return NextResponse.json(
+    //     {
+    //       error: "You must be logged in",
+    //     },
+    //     { status: 401 }
+    //   );
+    //}
+    const body = await req.json();
+    const { amount, topic, type } = quizCreationSchema.parse(body);
+
+    if (type !== "multiple_choice" && type !== "open_ended") {
+      console.log("Invalid game type");
+    }
+
+    type GameType = "multiple_choice" | "open_ended";
+    const game = await prisma.game.create({
+      data: {
+        game_type: type,
+        time_started: new Date(),
+        user_id: session?.user?.id,
+        topic: topic,
+      },
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          error: error.issues,
+        },
+        { status: 400 }
+      );
+    }
+  }
+}
---
+++ src/app/api/questions/route.ts
@@ -0,0 +1,65 @@
+import { strict_output } from "@/lib/gpt";
+import { getAuthSession } from "@/lib/nextauth";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+// POST /api/questions
+export const POST = async (req: Request, res: Response) => {
+  try {
+    // const session = await getAuthSession();
+    // if (!session) {
+    //   return NextResponse.json(
+    //     {
+    //       error: "You must be logged in to create a quiz",
+    //     },
+    //     { status: 401 }
+    //   );
+    // }
+    const body = await req.json();
+    const { amount, topic, type } = quizCreationSchema.parse(body);
+    let questions: any;
+    if (type === "open_ended") {
+      questions = await strict_output(
+        "You are an expert examiner, terse and clear in your objective, to generate open ended questions. speak specifically. do not deviate. do not be afraid to go deep. provide specific answers. use British English for your response. be clear and concise. It is important that you store all the questions and answers in a json array",
+
+        // add level of educational qualification later to narrow the question difficulty
+        new Array(amount).fill(
+          `generate open ended questions on this topic ${topic}.`
+        ),
+        { question: "question", answer: "answer with max lenght of 15 words" }
+      );
+    } else if (type === "multiple_choice") {
+      questions = await strict_output(
+        "You are an expert examiner, terse and clear in your objective, to generate multiple choice questions. speak specifically. do not deviate. do not be afraid to go deep. provide specific answers. use British English for your response. be clear and concise. It is important that you store all the questions and answers in a json array",
+
+        // add level of educational qualification later to narrow the question difficulty
+        new Array(amount).fill(
+          `generate open ended questions on this topic ${topic}.`
+        ),
+        {
+          question: "question",
+          answer: "answer with max lenght of 15 words",
+          option1: "1st option with max of 15 words",
+          option2: "2nd option with max of 15 words",
+          option3: "3rd option with max of 15 words",
+        }
+      );
+    }
+    return NextResponse.json(
+      {
+        questions,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          error: error.issues,
+        },
+        { status: 400 }
+      );
+    }
+  }
+};
